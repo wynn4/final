80c91627789317716306ba551d1153ea34ef5317
wynn4 <wynnsquad@gmail.com>
2017-04-25 11:30:42 -0600

    finished sort of

--- final/p1/p1.m
+++ final/p1/p1.m
@@ -1,18 +1,12 @@
 clc;
 clear;
 
-A = [0, 0, 1, 0;
-    0, 0, 0, 1;
-    0, -1, -0.1, 0.1;
-    0, 11, 0.1, -0.11];
-
-B = [0, 0;
-    0, 0;
-    0.1, -0.1;
-    -0.1, 1.1];
+%load the data file
+load('pendulum.mat')
 
 %problem 1a
-disp('part a')
+disp('__________part a_______________')
+
 
 B_tau_only = [0, 0;
               0, 0;
@@ -22,7 +16,7 @@ B_tau_only = [0, 0;
 %compute controllabiltiy matrix and check rank
 if rank(ctrb(A,B_tau_only)) ~= 4
     disp('not controllable using only Tau')
-    ctrb(A,B_tau_only)
+    controllability_matrix = ctrb(A,B_tau_only)
 else
     ctrb(A,B_tau_only)
 end
@@ -37,7 +31,8 @@ test_eig3 = rank([A - eigs(3,1)*eye(4) B_tau_only])
 test_eig4 = rank([A - eigs(4,1)*eye(4) B_tau_only])
 
 %problem 1b
-disp('part b')
+disp('__________part b_______________')
+
 
 
 C1 = [1, 0, 0, 0];
@@ -54,7 +49,50 @@ rank([A-eigs(3,1)*eye(4); C2])
 rank([A-eigs(4,1)*eye(4); C2])
 
 %problem 1c
+B = B(:,1);
 C = [0, 0, 1, 0; 0, 1, 0, 0];
-disp('part c')
+D = [0;0];
+disp('__________part c_______________')
 
-sys = ss(A,B,C,[0,0;0,0]);
+sys = ss(A,B,C,D);
+
+%check if observable
+if rank(obsv(A,C)) ~= 4,
+    disp('not observable')
+else
+    rank(obsv(A,C))
+end
+
+%not observable, so lets get the transmission zeros
+transmission_zeros = tzero(minreal(sys))
+
+%see where G(S) looses rank
+syms s
+s = 1;
+G = C*inv(s*eye(4)-A)*B + D;
+
+disp('__________part d_______________')
+
+C = eye(4);
+sys = ss(A,B,C,[0; 0; 0; 0]);
+t = 0:0.01:100;
+u = zeros(1,length(t));
+x0 = [0, 0, 0, 0]';
+[Y, T, X] = lsim(sys,u,t,x0);
+
+figure(1)
+subplot(4,1,1)
+plot(T,X(:,1))
+title('Question 1 Part d: State Values vs Time')
+ylabel('P')
+subplot(4,1,2)
+plot(T,X(:,2))
+ylabel('theta')
+subplot(4,1,3)
+plot(T,X(:,3))
+ylabel('Pdot')
+subplot(4,1,4)
+plot(T,X(:,4))
+ylabel('thetadot')
+xlabel('time (t)')
+
--- final/p4/p4.m
+++ final/p4/p4.m
@@ -2,7 +2,10 @@ clc;
 clear;
 close all;
 
+%load the data file
 load('f16_long.mat')
+
+disp('__________part a___________')
 
 A = Along
 B = Blong
@@ -10,11 +13,13 @@ B = Blong
 r = [1000; 100; 10; 7];
 q = 1;
 
+%normalize Q1 by the equilibrium value of the corresponding states
 Q1 = [1/500, 0, 0, 0;
     0, 1/degtorad(2.3), 0, 0;
     0, 0, 1/degtorad(17.2), 0;
     0, 0, 0, 1/0.5];
 
+%Scale R1 according to Bryson's rule:
 R1 = [1/5, 0;
       0, 1/degtorad(25)];
   
@@ -40,6 +45,8 @@ sys_lqr_4 = ss(A-B*K_lqr_4,B,eye(4),[0,0;0,0;0,0;0,0;]);
 %create the open-loop system
 sys = ss(A,B,eye(4),[0,0;0,0;0,0;0,0;]);
 
+eigs_of_A = eig(A)
+
 [Wn_lqr_1, Z_lqr_1] = damp(sys_lqr_1);
 [Wn_lqr_2, Z_lqr_2] = damp(sys_lqr_2);
 [Wn_lqr_3, Z_lqr_3] = damp(sys_lqr_3);
@@ -49,9 +56,115 @@ sys = ss(A,B,eye(4),[0,0;0,0;0,0;0,0;]);
 [Wn, Z] = damp(sys);
 
 %plot damping ratio vs osc freq
-plot(Wn,Z,Wn_lqr_1,Z_lqr_1,Wn_lqr_2,Z_lqr_2,Wn_lqr_3,Z_lqr_3,Wn_lqr_4,Z_lqr_4)
-legend('Open-Loop', 'LQR r = 1000', 'LQR r = 100', 'LQR r = 10', 'LQR r = 7')
+figure(1)
+plot(Wn(4,1),Z(4,1),'x',Wn_lqr_1(4,1),Z_lqr_1(4,1),'s',Wn_lqr_2(4,1),Z_lqr_2(4,1),'o',Wn_lqr_3(4,1),Z_lqr_3(4,1),'*',Wn_lqr_4(4,1),Z_lqr_4(4,1),'d')
+title('Damping Ratio vs Natural Frequency')
+xlabel('Wn (hz)')
+ylabel('Damping Ratio')
+legend('Open-Loop', 'LQR r = 1000', 'LQR r = 100', 'LQR r = 10', 'LQR r = 7','location','NW')
 
 
-  
-  
+disp('__________part b___________')
+R = [0.0355, 0; 0, .0001];
+
+K_lqr = lqr(A,B,Q,R);
+
+t_lqr = 0:0.01:15;
+t_ol = 0:0.01:100;
+u_lqr = zeros(2,length(t_lqr));
+u_ol = zeros(2,length(t_ol));
+x0 = [20, .01, -.01, .02];
+
+
+sys_lqr = ss(A-B*K_lqr,B,eye(4),[0,0;0,0;0,0;0,0;]);
+
+% lsim(sys_lqr,u,t,x0)
+
+% use lsim to simulate the f-16 with LQR
+[Y_lqr, T_lqr, X_lqr] = lsim(sys_lqr,u_lqr,t_lqr,x0);
+
+% use lsim to simulate the open-loop f-16
+[Y_ol, T_ol, X_ol] = lsim(sys,u_ol,t_ol,x0);
+
+% back out what the input was
+U = -K_lqr*X_lqr';
+
+%plot u(t) vs time
+figure(2)
+plot(t_lqr,U(1,:),'-.',t_lqr,U(2,:))
+title('P4 part b: Input U over time')
+xlabel('time (s)')
+ylabel('input')
+legend('throttle','elevator')
+
+%plot the states vs time for both LQR control and open-loop
+figure(3)
+subplot(4,1,1)
+plot(T_lqr,X_lqr(:,1))
+title('P4 part b: States under LQR over time')
+ylabel('Airspeed')
+
+subplot(4,1,2)
+plot(T_lqr,X_lqr(:,2))
+ylabel('alpha')
+
+subplot(4,1,3)
+plot(T_lqr,X_lqr(:,3))
+ylabel('pitch angle')
+
+subplot(4,1,4)
+plot(T_lqr,X_lqr(:,4))
+ylabel('pitch rate')
+xlabel('time (s)')
+
+figure(4)
+subplot(4,1,1)
+plot(T_ol,X_ol(:,1))
+title('P4 part b: Open-Loop states over time')
+ylabel('Airspeed')
+
+subplot(4,1,2)
+plot(T_ol,X_ol(:,2))
+ylabel('alpha')
+
+subplot(4,1,3)
+plot(T_ol,X_ol(:,3))
+ylabel('pitch angle')
+
+subplot(4,1,4)
+plot(T_ol,X_ol(:,4))
+ylabel('pitch rate')
+xlabel('time (s)')
+
+
+disp('__________part c___________')
+% only measure theta and alpha
+C = [0, 1, 0, 0; 0, 0, 1, 0];
+% noise = 1e-5*randn(size(t_lqr));
+W_noise = [1, 0; 0, 1e-5];
+W_disturbance = Blong*(1e-4*eye(2))*Blong';
+
+% Kalman filter design
+Kf = (lqr(A',C',W_disturbance, W_noise))'
+
+%create the Kalman filter ss system
+%sysKF = ss(A-Kf*C,[B Kf],eye(4),0*[B Kf]);  % Kalman filter estimator
+
+sys = ss(A,B,C,[0, 0; 0, 0]);
+
+reg_sys = reg(sys,K_lqr,Kf)
+
+%reg_sys = zpk(reg_sys)
+
+disp('__________part d___________')
+
+opt = stepDataOptions('StepAmplitude',10);
+[Y_step,T_step] = step(reg_sys, opt);
+
+figure(5)
+plot(T_step,Y_step(:,:,2))
+title('Step Response of Compensator system')
+xlabel('time (s)')
+ylabel('output')
+legend('delta phi', 'delta alpha','location','NW')
+
